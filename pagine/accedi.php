4aa56fac1941ef1d1f2c9c18cbbbb01171f004d2
Giulia1310 <giulia.gubellini@liceobanfi.eu>
2022-04-01 12:08:13 +0200

    form_finito

--- pagine/accedi.php
+++ pagine/accedi.php
@@ -121,24 +121,19 @@
 
 <!-- VIDEO SFONDO -->
     <div class="VideoAccedi">
-        <h3 class="med-text reveal">hai già un account IG?</h3>
-        
-        
-
-
 
     <div class="VideoAccedi__content reveal">
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-			<table class="form_accedi">
+			<table class="form_accedi" style="margin-top: 10px;">
 				<tr>
-					<td>Username:</td> <td><input type="text" name="username" value="<?php echo $username; ?>" required></td>
-				</tr>
+					<td>Username:</td> <td><input class="grandezzainput" type="text" name="username" value="<?php echo $username; ?>" required></td>
+				</tr><br>
 				<tr>
-					<td>Password:</td> <td><input type="password" name="password" required></td>
+					<td>Password:</td> <td><input class="grandezzainput" type="password" name="password" required></td>
 				</tr>
             </table>
 
-			<p><input type="submit" value="Login"></p>
+			<br><br><p><input class="grandezzainput" type="submit" value="ACCEDI"></p>
 		</form>
 		<?php
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -172,7 +167,10 @@
 					}
 				}
 			}
-			?>	
+			?>
+            
+            <h3 class="med-text"  style="margin-top: 20px;">Non hai ancora un account?<br><br>REGISTRATI ORA! </h3>
+            
     </div>
 
 
