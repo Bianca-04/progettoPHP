13e7c61c1941bd6c069196c53f3ce861b55612a7
Giulia1310 <giulia.gubellini@liceobanfi.eu>
2022-04-01 10:35:37 +0200

    aggiunto_form_dasistemare

--- pagine/accedi.php
+++ pagine/accedi.php
@@ -1,3 +1,30 @@
+<?php
+	session_start();
+	
+
+	require('../data/connessione_database.php');
+    echo session_id();
+
+	if(isset($_SESSION['username'])){
+		header('location: pagine/home.php');
+	}
+
+	if(isset($_POST["username"])){
+		$username = $_POST["username"];
+	}
+	else{
+		$username = "";
+	}
+	
+	if (isset($_POST["password"])){
+		$password = $_POST["password"];
+	}
+	else {
+		$password = "";
+	}
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -44,16 +71,19 @@
             </li>
         </ul>
 
-        <div class="cta">
+<!-- TOGLIERE IL BOTTONE -->
+        <!-- <div class="cta">
             <a href="../pagine/accedi.php" class="buttona">Accedi</a>
         </div>
         <div class="hamburger">
             <span></span>
             <span></span>
             <span></span>
-        </div>
+        </div> -->
+
     </div>
-    <!-- Jquery -->
+
+    <!-- Jquery - FORM CHE SCENDE -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
@@ -65,27 +95,7 @@
         });
     </script>
 
-    <div class="VideoAccedi">
-        <h3 class="med-text reveal">hai già un account IG?</h3>
-
-        <div class="VideoAccedi__content reveal">
-            <ul class="normal-text reveal">indirizzo e-mail:<input type="text" name="indirizzo"></ul>
-            <ul class="normal-text reveal">password:<input type="password"><button onclick="lavatrice()">login</button></ul>
-            <div class="passdim reveal"><a href="../pagine/password.php">recupera password</a></div>
-        </div>
-
-        <video autoplay muted loop id="VideoIscriviti">
-            <source src="../immagini/VideoAccedi.mp4" type="video/mp4">
-          </video>
-    </div>
-
-    <script>
-        function lavatrice() {
-            document.write("azione avvenuta con successo, tornare nella home premendo la freccina in alto a sinistra");
-
-        }
-    </script>
-    <script>
+<script>
         $(document).ready(function() {
             /* Open Panel */
             $(".hamburger").on('click', function() {
@@ -108,6 +118,78 @@
             mobile: false,
         });
     </script>
+
+<!-- VIDEO SFONDO -->
+    <div class="VideoAccedi">
+        <h3 class="med-text reveal">hai già un account IG?</h3>
+        
+        
+
+
+
+    <div class="VideoAccedi__content reveal">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+			<table class="form_accedi">
+				<tr>
+					<td>Username:</td> <td><input type="text" name="username" value="<?php echo $username; ?>" required></td>
+				</tr>
+				<tr>
+					<td>Password:</td> <td><input type="password" name="password" required></td>
+				</tr>
+            </table>
+
+			<p><input type="submit" value="Login"></p>
+		</form>
+		<?php
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				if( empty($_POST["username"]) or empty($_POST["password"])) {
+					echo "<p>Completare tutti i campi con *</p>"; //METTERE O MAIL O TELEFONO, CHE NON DEVONO GIA ESISTERE
+				} else {
+					$conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
+					if($conn->connect_error){
+						die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
+					}
+					
+                    $utente = $_POST["username"];
+					//sistemare questo from
+					$myquery = "SELECT username, password 
+								FROM $utente
+								WHERE username='$username'
+									AND password='$password'";
+
+					$ris = $conn->query($myquery) or die("<p>Query fallita! ".$conn->error."</p>");
+
+					if($ris->num_rows == 0){
+						echo "<p>Utente non trovato o password errata</p>";
+						$conn->close();
+					} 
+					else {
+						$_SESSION["username"]=$username;
+												
+						$conn->close();
+						header("location: pagine/home.php");
+
+					}
+				}
+			}
+			?>	
+    </div>
+
+
+
+        <!-- FORM CHE BISOGNA CAPIRE SE RIMETTERE DENTRO AL VIDEO -->
+        <!-- <div class="VideoAccedi__content reveal">
+            <ul class="normal-text reveal">indirizzo e-mail:<input type="text" name="indirizzo"></ul>
+            <ul class="normal-text reveal">password:<input type="password"><button onclick="lavatrice()">login</button></ul>
+            <div class="passdim reveal"><a href="../pagine/password.php">recupera password</a></div>
+        </div> -->
+
+        <video autoplay muted loop id="VideoIscriviti">
+            <source src="../immagini/VideoAccedi.mp4" type="video/mp4">
+          </video>
+    </div>
+
+
 </body>
 
 </html>
