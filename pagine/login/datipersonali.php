82f7a18afa29e9a31345514eb2cc67eb3f81364c
gggiorginaaa <giorgia.passoni@liceobanfi.eu>
2022-04-11 14:45:51 +0200

    aggiunta pagine dati e acquisti personali

--- pagine/login/datipersonali.php
+++ pagine/login/datipersonali.php
@@ -1,22 +1,10 @@
-<?php
-	session_start();
-
-	require_once('../../data/connessione_database.php');
-
-	// if(!isset($_SESSION['username'])){
-	// 	header('location: ../index.php');
-	// }
-	$username = $_SESSION["username"];
-	
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina personale</title>
+    <title>Dati personali</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==" crossorigin="anonymous" />
     <link rel="stylesheet" href="../../style.css">
@@ -25,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="header">
+<div class="header">
         <div class="logo">
             <img src="../../immagini/logo.png" alt="immagine non disponibile">
         </div>
@@ -62,49 +50,6 @@
             <span></span>
         </div>
     </div>
-
-    <img src="../../immagini/sfondo_account" alt="L'immagine non è disponibile">
-    <div class="fotoaccount">
-        <div>
-            <h3 class="big-text reveal">BENVENUTO!</h3>
-            
-            <?php
-			$conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
-			if($conn->connect_error){
-				die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
-			}
-			
-			$sql = "SELECT username, nome, cognome 
-					FROM utente
-					WHERE username='$username'";
-			//echo $sql;
-			$ris = $conn->query($sql) or die("<p>Query fallita!</p>");
-			foreach($ris as $riga){ 
-			echo "<p>".$riga["nome"]." ".$riga["cognome"]."</p>";}
-		    ?>
-        </div>
-        <img src="../../immagini/sfondo_account.jpeg" alt="L'immagine non è disponibile">
-    </div>
-
-    <div class="spazio"></div>
-    <div class="tabellaaccount">
-				<div class="container">
-					<figure><img src="../../immagini/dati.jpeg"><figcaption><div class="figurecaption"><p><b><a href="datipersonali.php">I TUOI DATI</a></b></p></div></figcaption></figure>
-
-                </div>
-                <div class="container">
-					<figure><img src="../../immagini/carrello.jpeg"><figcaption><div class="figurecaption"><p><b><a href="carrello.php">CARRELLO</a></b></p></div></figcaption></figure>
-
-                </div>
-                <div class="container">
-					<figure><img src="../../immagini/acquisti.jpeg"><figcaption><div class="figurecaption"><p><b><a href="acquistipersonali.php">I TUOI ACQUISTI</a></b></p></div></figcaption></figure>
-
-                </div>
-				
-    </div>
-    <div class="spazio"></div>
-    <br> 
-
-
+    
 </body>
 </html>
